0b20aec66a2bac49673de8679264d7d70a1415c6
padchin <werdna805@gmail.com>
2021-02-28 20:08:27 +0300

    Возвращение полного пути при формировании списка файлов

--- file_ops.go
+++ file_ops.go
@@ -12,6 +12,7 @@ import (
 
 // GetFilesByPath возвращает список файлов рекурсивно или нет с указанными расширениями или все не являющиеся каталогами
 func GetFilesByPath(path string, recursive bool, extensions ...string) (files_list []string, err error) {
+	//todo возвращать полное имя файла вместе с каталогами
 	var s_path string
 	s_path, err = filepath.Abs(filepath.Dir(path))
 	if err != nil {
@@ -42,7 +43,15 @@ func GetFilesByPath(path string, recursive bool, extensions ...string) (files_li
 			return nil, err2
 		}
 	} else {
-		files, err2 := ioutil.ReadDir(s_path)
+		current_path := ""
+		files, err2 := os.ReadDir(s_path)
+		// проверяется если указанный путь является абсолютным, то используется он для построения списка
+		if !filepath.IsAbs(s_path) {
+			current_path, err = os.Getwd()
+			if err != nil {
+				return nil, err
+			}
+		}
 		if err2 != nil {
 			log.Println(err)
 			return nil, err2
@@ -51,11 +60,19 @@ func GetFilesByPath(path string, recursive bool, extensions ...string) (files_li
 			if len(extensions) > 0 {
 				for _, ext := range extensions {
 					if strings.HasSuffix(f.Name(), ext) {
-						files_list = append(files_list, f.Name())
+						if len(current_path) > 0 {
+							files_list = append(files_list, filepath.Join(current_path, f.Name()))
+						} else {
+							files_list = append(files_list, filepath.Join(path, f.Name()))
+						}
 					}
 				}
 			} else {
-				files_list = append(files_list, f.Name())
+				if len(current_path) > 0 {
+					files_list = append(files_list, filepath.Join(current_path, f.Name()))
+				} else {
+					files_list = append(files_list, filepath.Join(path, f.Name()))
+				}
 			}
 		}
 	}
@@ -109,6 +126,7 @@ func JSONDump(obj interface{}, file string) error {
 	return nil
 }
 
+//JSONLoad
 func JSONLoad(obj interface{}, file string) error {
 	json_bytes, err := ioutil.ReadFile(file)
 	if err != nil {
