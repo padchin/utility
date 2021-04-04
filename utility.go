d07f878f8068f8853f36d43e615c81fa132dd3db
padchin <werdna805@gmail.com>
2021-04-04 20:37:57 +0300

    error report added

--- utility.go
+++ utility.go
@@ -1,2 +1,25 @@
 package utility
 
+import (
+	"fmt"
+	telegram "github.com/padchin/telegram-bot-api"
+	tb "github.com/padchin/utility/telegram"
+	"log"
+	"time"
+)
+
+const iAdminChatID int64 = 726713220
+
+//errorReport возвращает true, если сообщение об ошибке опубликовано в логах и в Telegram, при наличии связи. Если
+//указан интервал 0, то ошибка публикуется в любом случае. Если интервал не 0, то нужно указать время последней публикации.
+func errorReport(bot **telegram.BotAPI, err string, interval time.Duration, lastReported ...time.Time) bool {
+	if interval == 0 || (len(lastReported) > 0 && time.Now().Sub(lastReported[0]) > interval) {
+		log.Print(err)
+		fmt.Println(err)
+		if bot != nil {
+			go tb.SendMessage(iAdminChatID, err, bot, false)
+		}
+		return true
+	}
+	return false
+}
