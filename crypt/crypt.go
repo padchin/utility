34281d06999beeadc6eb3daa7ec328f5e5ebc42e
padchin <werdna805@gmail.com>
2021-04-30 16:01:52 +0300

    encrypt decrypt strings

--- crypt/crypt.go
+++ crypt/crypt.go
@@ -1,12 +1,16 @@
 package crypt
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/md5"
+	crypto_rand "crypto/rand"
+	"encoding/hex"
 	"fmt"
 	"golang.org/x/crypto/bcrypt"
 	"io"
 	"io/ioutil"
-	"math/rand"
+	math_rand "math/rand"
 	"os"
 	"time"
 )
@@ -30,6 +34,7 @@ func ComputeMD5(path string) (string, error) {
 func CheckPassword(password string, hash ...string) (bool, error) {
 	var _hash []byte
 	var err error
+
 	if len(hash) == 0 {
 		_hash, err = secretHashRead()
 		if err != nil {
@@ -40,34 +45,41 @@ func CheckPassword(password string, hash ...string) (bool, error) {
 	}
 	// Comparing the password with the hash
 	err = bcrypt.CompareHashAndPassword(_hash, []byte(password))
+
 	if err != nil {
 		return false, err
 	}
+
 	return true, nil
 }
 
 // GeneratePasswordAndStoreNewHash вызывает passPhraseGen и записывает новый хэш пароля на диск и возвращает сгенерированный пароль
 // из простых символов ascii с некоторым исключением для удобства набора на клавиатуре.
 func GeneratePasswordAndStoreNewHash() ([]byte, error) {
-	passPhrase := passPhraseGen(12)
-	byteHash, errHash := bcrypt.GenerateFromPassword(passPhrase, 10)
-	if errHash != nil {
-		return nil, errHash
+	phrase := passPhraseGen(12)
+	hash, err := bcrypt.GenerateFromPassword(phrase, 10)
+
+	if err != nil {
+		return nil, err
 	}
-	errWrite := secretHashWrite(&byteHash)
-	if errWrite != nil {
-		return nil, errWrite
-	} else {
-		return passPhrase, nil
+
+	err = secretHashWrite(&hash)
+
+	if err != nil {
+		return nil, err
 	}
+
+	return phrase, nil
 }
 
 func passPhraseGen(iPhraseLength int) []byte {
 	var bytePassPhrase []byte
-	rand.Seed(time.Now().UnixNano())
+
+	math_rand.Seed(time.Now().UnixNano())
+
 	for i := 1; i <= iPhraseLength; i++ {
 	Gen:
-		uiRandNumber := uint8(rand.Intn(125-33) + 33)
+		uiRandNumber := uint8(math_rand.Intn(125-33) + 33)
 		switch uiRandNumber {
 		// убираются некоторые неудобные символы
 		case 34, 39, 44, 45, 46, 94, 96, 124:
@@ -77,19 +89,103 @@ func passPhraseGen(iPhraseLength int) []byte {
 			bytePassPhrase = append(bytePassPhrase, uiRandNumber)
 		}
 	}
+
 	return bytePassPhrase
 }
 
 func secretHashRead() ([]byte, error) {
 	secret, errRead := ioutil.ReadFile("secret")
+
 	if errRead != nil {
 		return nil, errRead
 	}
+
 	return secret, nil
 }
 
 // secretHashWrite записывает хэш в файл secret текущего каталога.
 func secretHashWrite(secret *[]byte) error {
 	errWrite := ioutil.WriteFile("secret", *secret, 0600)
+
 	return errWrite
+}
+
+func Encrypt(stringToEncrypt string, keyString string) (encryptedString string) {
+
+	//Since the key is in string, we need to convert decode it to bytes
+	key, _ := hex.DecodeString(keyString)
+	plaintext := []byte(stringToEncrypt)
+
+	//Create a new Cipher Block from the key
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	//Create a new GCM - https://en.wikipedia.org/wiki/Galois/Counter_Mode
+	//https://golang.org/pkg/crypto/cipher/#NewGCM
+	aesGCM, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	//Create a nonce. Nonce should be from GCM
+	nonce := make([]byte, aesGCM.NonceSize())
+	if _, err = io.ReadFull(crypto_rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+
+	//Encrypt the data using aesGCM.Seal
+	//Since we don't want to save the nonce somewhere else in this case, we add it as a prefix to the encrypted data. The first nonce argument in Seal is the prefix.
+	ciphertext := aesGCM.Seal(nonce, nonce, plaintext, nil)
+	return fmt.Sprintf("%x", ciphertext)
+}
+
+func Decrypt(encryptedString string, keyString string) (decryptedString string) {
+
+	key, _ := hex.DecodeString(keyString)
+	enc, _ := hex.DecodeString(encryptedString)
+
+	//Create a new Cipher Block from the key
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	//Create a new GCM
+	aesGCM, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	//Get the nonce size
+	nonceSize := aesGCM.NonceSize()
+
+	//Extract the nonce from the encrypted data
+	nonce, ciphertext := enc[:nonceSize], enc[nonceSize:]
+
+	//Decrypt the data
+	plaintext, err := aesGCM.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return fmt.Sprintf("%s", plaintext)
+}
+
+func main() {
+
+	bytes := make([]byte, 32) //generate a random 32 byte key for AES-256
+	if _, err := crypto_rand.Read(bytes); err != nil {
+		panic(err.Error())
+	}
+
+	key := hex.EncodeToString(bytes) //encode key in bytes to string and keep as secret, put in a vault
+	fmt.Printf("key to Encrypt/Decrypt : %s\n", key)
+
+	encrypted := Encrypt("Hello Encrypt", key)
+	fmt.Printf("encrypted : %s\n", encrypted)
+
+	decrypted := Decrypt(encrypted, key)
+	fmt.Printf("decrypted : %s\n", decrypted)
 }
