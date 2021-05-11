16744bb6af88fa8e6170c2c50d756e235aeb081b
padchin <werdna805@gmail.com>
2021-05-11 18:03:28 +0300

    LocalTimeFormat

--- time/time.go
+++ time/time.go
@@ -9,7 +9,9 @@ import (
 func LocalTimeFormatFromUnix(unixTime int, format ...string) string {
 	loc, _ := time.LoadLocation("Europe/Moscow")
 	time.Local = loc
+
 	t := time.Unix(int64(unixTime), 0).Local()
+
 	if len(format) > 0 {
 		return t.Format(format[0])
 	} else {
@@ -22,7 +24,9 @@ func LocalTimeFormatFromUnix(unixTime int, format ...string) string {
 func LocalTimeFormatFromUnixNano(unixTime int, format ...string) string {
 	loc, _ := time.LoadLocation("Europe/Moscow")
 	time.Local = loc
+
 	t := time.Unix(int64(unixTime/1e9), int64(unixTime%1e9)).Local()
+
 	if len(format) > 0 {
 		return t.Format(format[0])
 	} else {
@@ -30,9 +34,17 @@ func LocalTimeFormatFromUnixNano(unixTime int, format ...string) string {
 	}
 }
 
-// LocalTimeFormat возвращает форматированную строку текущего времени
-func LocalTimeFormat() string {
+// LocalTimeFormat возвращает форматированную строку текущего времени в формате 02.01.2006 15:04:05,
+// если не указан формат, или в соответствии с указанным форматом в местном времени (+3)
+func LocalTimeFormat(format ...string) string {
 	loc, _ := time.LoadLocation("Europe/Moscow")
 	time.Local = loc
-	return time.Now().Local().Format("02.01.2006 15:04:05")
+
+	t := time.Now().Local()
+
+	if len(format) > 0 {
+		return t.Format(format[0])
+	} else {
+		return t.Format("02.01.2006 15:04:05")
+	}
 }
