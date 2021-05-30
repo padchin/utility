fcea28be08671aafdcb4059861b16fa865cc84f6
padchin <werdna805@gmail.com>
2021-05-30 11:39:41 +0300

    fixes

--- remote/remote.go
+++ remote/remote.go
@@ -3,36 +3,44 @@ package remote
 import (
 	"context"
 	"errors"
-	"fmt"
 	"os/exec"
 	"runtime"
 	"sync"
 	"time"
 )
 
+var ErrWrongOS = errors.New("SecureCopyLinux is only supported on UNIX systems")
+
 // SecureCopyLinux копирует файл по сети с использованием scp. Работает только в системе Unix. Таймаут указывается
 // в секундах. Также нужно передать указатели для блокировки ресурсов при совместном доступе или nil при отсутствии
 // необходимости блокировки.
 func SecureCopyLinux(src string, dest string, timeout int, srcLocker *sync.RWMutex, destLocker *sync.RWMutex) error {
 	if runtime.GOOS != "linux" {
-		return fmt.Errorf("SecureCopyLinux: is only supported on unix systems")
+		return ErrWrongOS
 	}
+
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeout)*time.Second)
 	defer cancel()
+
 	if srcLocker != nil {
 		srcLocker.RLock()
 		defer srcLocker.RUnlock()
 	}
+
 	if destLocker != nil {
 		destLocker.Lock()
 		defer destLocker.Unlock()
 	}
-	_, err := exec.CommandContext(ctx, "scp", "-o ConnectTimeout=30", src, dest).Output()
+
+	err := exec.CommandContext(ctx, "scp", "-o ConnectTimeout=30", src, dest).Run()
+
 	if errors.Is(ctx.Err(), context.DeadlineExceeded) {
 		return context.DeadlineExceeded
 	}
+
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
