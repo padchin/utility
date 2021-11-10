85549009baed9a42e6fc85cbd379262996e1c182
padchin <werdna805@gmail.com>
2021-11-10 10:30:09 +0300

    fixes

--- math/math.go
+++ math/math.go
@@ -9,20 +9,18 @@ func Round(num float64, precision int) float64 {
 	return float64(int(num*output+math.Copysign(0.5, num*output))) / output
 }
 
-// FastPower возвращает x в степени y. Возвращает 0 для отрицательных степеней.
-func FastPower(x, y int) int {
-	if y < 0 {
-		return 0
-	}
-
+// FastPower возвращает x в степени n. Возвращает 0 для отрицательных степеней.
+func FastPower(x, n int) int {
 	switch {
-	case y == 0:
+	case n < 0:
+		return 0
+	case n == 0:
 		return 1
-	case y == 1:
+	case n == 1:
 		return x
-	case y%2 == 0:
-		return FastPower(x*x, y/2)
+	case n%2 == 0:
+		return FastPower(x*x, n/2)
 	default:
-		return x * FastPower(x*x, (y-1)/2)
+		return x * FastPower(x*x, (n-1)/2)
 	}
 }
--- series/arrays.go
+++ series/arrays.go
@@ -2,23 +2,24 @@ package series
 
 // MaxValueElementsCount возвращает максимальный по значению элемент и их количество.
 func MaxValueElementsCount(array *[]int) (int, int) {
+	// aiBias содержит значения массива в качестве ключа и кол-во повторений элемента массива в качестве значения
 	aiBias := make(map[int]int)
+
 	for _, x := range *array {
-		if //goland:noinspection GoLinterLocal
-		_, ok := aiBias[x]; ok {
-			aiBias[x]++
-		} else {
-			aiBias[x] = 1
+		aiBias[x]++
+	}
+
+	// находим максимум
+	// берем любой элемент из словаря как максимальный
+	val := aiBias[(*array)[0]]
+	count := 0
+
+	for v, c := range aiBias {
+		if c > val {
+			val = c
+			count = v
 		}
 	}
-	//find maximum
-	maxVal := 0
-	valuesCount := 0
-	for keyValue, count := range aiBias {
-		if count > maxVal {
-			maxVal = count
-			valuesCount = keyValue
-		}
-	}
-	return maxVal, valuesCount
+
+	return val, count
 }
